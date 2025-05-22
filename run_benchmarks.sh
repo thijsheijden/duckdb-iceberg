68134c2bafc6c8d6cc9b32d2122ed2a3676d3419
thijsheijden <hi@thijsheijden.nl>
2025-05-22 12:44:13 +0200

    Add 100k filesize benchmarks

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -1,31 +1,20 @@
 BASE_CMD="build/release/benchmark/benchmark_runner --root-dir ."
+FILE_COUNTS=(1k 10k 100k)
 
 # Regular Iceberg benchmarks
-# 1k files
-# shellcheck disable=SC2012
-bench_count=$(ls -1 benchmark/regular/ordered/1k/*.benchmark | wc -l)
-for i in $(seq 1 "${bench_count}"); do
-  ${BASE_CMD} benchmark/regular/ordered/1k/"${i}".benchmark
-done
-
-# 10k files
-# shellcheck disable=SC2012
-bench_count=$(ls -1 benchmark/regular/ordered/10k/*.benchmark | wc -l)
-for i in $(seq 1 "${bench_count}"); do
-  ${BASE_CMD} benchmark/regular/ordered/10k/"${i}".benchmark
+for i in "${FILE_COUNTS[@]}"; do
+  # shellcheck disable=SC2012
+  bench_count=$(ls -1 benchmark/regular/ordered/"${i}"/*.benchmark | wc -l)
+  for c in $(seq 1 "${bench_count}"); do
+    ${BASE_CMD} benchmark/regular/ordered/"${i}"/"${c}".benchmark
+  done
 done
 
 # BF_EDS benchmarks
-# 1k files
 # shellcheck disable=SC2012
-bench_count=$(ls -1 benchmark/bf_eds/ordered/1k/*.benchmark | wc -l)
-for i in $(seq 1 "${bench_count}"); do
-  ${BASE_CMD} benchmark/bf_eds/ordered/1k/"${i}".benchmark
-done
-
-# 10k files
-# shellcheck disable=SC2012
-bench_count=$(ls -1 benchmark/bf_eds/ordered/10k/*.benchmark | wc -l)
-for i in $(seq 1 "${bench_count}"); do
-  ${BASE_CMD} benchmark/bf_eds/ordered/10k/"${i}".benchmark
+for i in "${FILE_COUNTS[@]}"; do
+  bench_count=$(ls -1 benchmark/bf_eds/ordered/"${i}"/*.benchmark | wc -l)
+  for c in $(seq 1 "${bench_count}"); do
+    ${BASE_CMD} benchmark/bf_eds/ordered/"${i}"/"${c}".benchmark
+  done
 done
