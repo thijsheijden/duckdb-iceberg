895cd0d2d1401dc6140896436933ee54443e3148
thijsheijden <hi@thijsheijden.nl>
2025-06-09 15:31:53 +0200

    Update test generation script
    - Now uniformly generates test ranges at all base 2 sizes (2^1 -> 2^63)

--- test/sql/BF_EDS/generate_tests.py
+++ test/sql/BF_EDS/generate_tests.py
@@ -1,10 +1,11 @@
 import argparse
 import json
 import math
-import msvcrt
 import os.path
 import random
 import shutil
+import matplotlib.pyplot as plt
+import numpy as np
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', default=100, type=int) # The number of testcases to generate
@@ -18,8 +19,7 @@ random.seed(args.seed)
 
 # Remove existing tests
 if len(os.listdir(args.test_out_dir)) > 0:
-    print("Previously generated tests found, remove tests and generate new tests? (y/n)")
-    ans = msvcrt.getwch()
+    ans = input("Previously generated tests found, remove tests and generate new tests? (y/n)")[0]
     if ans == 'y':
         shutil.rmtree(args.test_out_dir)
         os.mkdir(args.test_out_dir)
@@ -70,43 +70,58 @@ CREATE SECRET bf_eds_nc_keys (
 '''
     )
 
-
-# Calculate log_2 of range max
-# range_max_lg2 = math.log2(9223372036854775807)
-
-# Determine how many ranges to generate per power of 2
-# ranges_per_2_power = int(math.ceil(args.c / range_max_lg2))
-# cur_pow_2 = 1
+def generate_uniform_log_ranges(ranges_per_bucket, max_log=63):
+    ranges = []
+    max_range_val = (1 << max_log)
+    for log_size in range(1, max_log + 1):
+        size = (1 << log_size) - 1
+        for _ in range(ranges_per_bucket):
+            start = random.randint(0, max_range_val - size)
+            end = start + size
+            ranges.append({'min': start, 'max': end})
+            testcase_range_deltas.append(end - start)
+    return ranges
 
 # Generate a number of testcases with ranges based on some seed
-testcase_ranges = []
-for testcase_idx in range(args.c):
-    # Create output file
+testcase_range_deltas = [] # The delta of each testcase range
+testcases_per_log2 = int(args.c / 63)
+ranges = generate_uniform_log_ranges(testcases_per_log2, 63)
+testcase_idx = 0
+for r in ranges:
     with open(os.path.join(args.test_out_dir, f"test_{testcase_idx}.test"), 'w') as out_f:
         add_header(out_f, testcase_idx)
-
-        # cur_max = (pow(2, cur_pow_2)) - 1
-        cur_max = 9223372036854775807
-        print(f'Generating test {testcase_idx}...')
-        range_min = random.randint(0, cur_max)
-        range_max = random.randint(range_min, cur_max)
-
-        testcase_ranges.append({'min': range_min, 'max': range_max})
-
-        # Generate random range
         out_f.write(
-            f'''query I
-select count(*) from my_datalake.default.filtering_using_query_token where value between {range_min} and {range_max};
+                f'''query I
+select count(*) from my_datalake.default.filtering_using_query_token where value between {r['min']} and {r['max']};
 ----
 0
-            '''
+                '''
+            )
 
-        )
+        testcase_idx += 1
 
-        # if testcase_idx % ranges_per_2_power == 0:
-        #     cur_pow_2 += 1
+
+# Plot distribution of range deltas
+# Convert to numpy array
+deltas = np.array(testcase_range_deltas, dtype=np.float64)
+log_bins = np.floor(np.log2(deltas)).astype(int) # Compute log2 of each delta and floor to get bin index
+bins = np.arange(0, 65)  # Bins from log2(1) to log2(2^64)
+
+# Plot histogram
+plt.style.use('physrev.mplstyle') # Add Gnuplot styling
+plt.rcParams['figure.dpi'] = "300"
+plt.figure(figsize=(20, 6))
+plt.hist(log_bins, bins=bins, align='left', rwidth=2)
+plt.xticks(bins)
+plt.title(f"Query Range Size Histogram ($log_2(size)$) For {args.c} Randomly Generated Ranges", size=16, pad=16)
+plt.xlabel("Range Size", size=12)
+plt.ylabel("Frequency", size=12)
+# plt.clabel(f"Seed: {args.seed}")
+plt.savefig(fname=f'query_range_test_deltas_{args.table_name}.png')
+
+print(f'Generated {testcases_per_log2} testcases per log_2 for {testcases_per_log2 * 63} total testcases')
 
 # Write all the test case ranges to a separate file which contains a dictionary of testcase -> testcase range
 with open(os.path.join(args.test_out_dir, "test_ranges.json"), 'w') as test_ranges_f:
-    json.dump(testcase_ranges, test_ranges_f)
+    json.dump(ranges, test_ranges_f)
 
